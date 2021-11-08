c6406b76cab8f207baef73a8da2709c65ff06c8e
renydiaz02 <reny_diaz@ucne.edu.do>
2021-11-08 16:43:54 -0400

    Creando Consulta de TiposTareas

---
+++ UI/Consultas/cTiposTareas.xaml.cs
@@ -0,0 +1,44 @@
+﻿using P2_AP1_Reny20190003.BLL;
+using P2_AP1_Reny20190003.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P2_AP1_Reny20190003.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for cTiposTareas.xaml
+    /// </summary>
+    public partial class cTiposTareas : Window
+    {
+        public cTiposTareas()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<TiposTareas>();
+
+            switch (FiltroComboBox.SelectedIndex)
+            {
+                case 0: //Listado
+                    listado = TiposTareasBLL.GetTiposTarea();
+                    break;
+            }
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+        }
+    }
+}
