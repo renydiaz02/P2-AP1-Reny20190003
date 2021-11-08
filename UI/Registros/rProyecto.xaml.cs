41e6c0f8e9f6ea63060db25ad47050bb813062d7
renydiaz02 <reny_diaz@ucne.edu.do>
2021-11-08 17:23:36 -0400

    Subiendo el combobox

--- UI/Registros/rProyecto.xaml.cs
+++ UI/Registros/rProyecto.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using P2_AP1_Reny20190003.BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,40 @@ namespace P2_AP1_Reny20190003.UI.Registros
         public rProyecto()
         {
             InitializeComponent();
+            TipoTareaComboBox.ItemsSource = TiposTareasBLL.GetTiposTarea();
+            TipoTareaComboBox.SelectedValuePath = "TipoTareaId";
+            TipoTareaComboBox.DisplayMemberPath = "Descripcion";
+
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AgregarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoverFilaButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
