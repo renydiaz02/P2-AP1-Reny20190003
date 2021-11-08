dd81df579afc9065ca952ddb8ae5964811d9bd6e
renydiaz02 <reny_diaz@ucne.edu.do>
2021-11-08 16:23:11 -0400

    Creando la entidad TiposTareas

---
+++ Entidades/TiposTareas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_AP1_Reny20190003.Entidades
+{
+    public class TiposTareas
+    {
+        [Key]
+
+        public int TipoTareaId { get; set; }
+        public string Descripcion { get; set; }
+        public int Tiempo { get; set; }
+    }
+}
