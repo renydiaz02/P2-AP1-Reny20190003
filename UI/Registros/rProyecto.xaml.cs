42815134c43947fc6bc984559dea2c5c6b883be3
renydiaz02 <reny_diaz@ucne.edu.do>
2021-11-08 17:08:22 -0400

    Agregando Imagenes a los botones

---
+++ UI/Registros/rProyecto.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P2_AP1_Reny20190003.UI.Registros
+{
+    /// <summary>
+    /// Interaction logic for rProyecto.xaml
+    /// </summary>
+    public partial class rProyecto : Window
+    {
+        public rProyecto()
+        {
+            InitializeComponent();
+        }
+    }
+}
